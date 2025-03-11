1ae3f5ad133fce72344990bd9d08cb1137993a2d
Atanu Shil <atanushil358@gmail.com>
2025-03-12 03:04:22 +0530

    created the dashboard overview

--- ProjectPilot/src/components/Layout/Layout.jsx
+++ ProjectPilot/src/components/Layout/Layout.jsx
@@ -7,15 +7,21 @@ import Profile from "../ui/Profile";
 
 export const Layout = () => {
   return (
-    <div className="w-screen">
-      <div className="grid grid-cols-12 grid-rows-8  bg-slate-400  ">
-        <div className="col-span-3 row-span-8 "><SideBar/></div>
-        <div className="col-span-8 col-start-4 flex justify-end items-center px-5 "><SearchBar/></div>
-        <div className="col-span-9 row-span-7 col-start-4 row-start-2 bg-amber-200 text-black"><Outlet/></div>
-        <div className="col-span-1 col-start-12 row-start-1 flex justify-between px-1 items-center ">
+    <div className="h-screen w-screen overflow-hidden">
+      <div className="grid grid-cols-12 grid-rows-8 h-full w-full bg-slate-400">
+        <div className="col-span-3 row-span-8">
+          <SideBar />
+        </div>
+        <div className="col-span-8 col-start-4 flex justify-end items-center px-5">
+          <SearchBar />
+        </div>
+        <div className="col-span-9 row-span-7 col-start-4 row-start-2 bg-amber-200 text-black">
+          <Outlet />
+        </div>
+        <div className="col-span-1 col-start-12 row-start-1 flex justify-between px-1 items-center">
           <Notification />
-          <Profile/>
-          </div>
+          <Profile />
+        </div>
       </div>
     </div>
   );
