c6bf436aebe9e473608ad6d90d1b18a196e98e0b
Atanu Shil <atanushil358@gmail.com>
2025-03-12 02:55:03 +0530

    fixed the bug which occured an issue in deployement

--- ProjectPilot/src/utils/index.js
+++ ProjectPilot/src/utils/index.js
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout/Layout";
 import ProductBacklog from "../components/product-backlog";
 import Dashboard from "../components/dashboard";
-import { SprintBacklog } from "../components/sprint-backlog/Index";
+import { SprintBacklog } from "../components/sprint-backlog";
 import { ScrumMeetings } from "../components/srucm-meeting";
 import { TestingBugs } from "../components/testing-bugs";
 import { Deployment } from "../components/deployment-version";
